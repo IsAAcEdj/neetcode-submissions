1e27495fa1ab6dc9817c3397c5f96e007c563f72
Isaac Edward <xxxrakexxx@gmail.com>
2026-09-08 12:51:21 -0400

    Add: valid-sudoku - submission-2

---
+++ Data Structures & Algorithms/valid-sudoku/submission-2.py
@@ -0,0 +1,31 @@
+class Solution:
+    from collections import defaultdict
+    def isValidSudoku(self, board: List[List[str]]) -> bool:
+        
+        for i in range(9):
+            dicti = defaultdict(int)
+            dictj = defaultdict(int)
+            for j in range(9):
+                if board[i][j] != ".":
+                    dicti[board[i][j]] += 1
+                if(dicti[board[i][j]] > 1):
+                    print("1")
+                    print(dicti, dictj)
+                    return False
+                if board[j][i] != ".":
+                    dictj[board[j][i]] += 1
+                if(dictj[board[j][i]] > 1):
+                    print(dicti, dictj)
+                    return False
+        for i in range(3):
+            for f in range (3):
+                dicti.clear()
+                for j in range(3):
+                    for k in range(3):
+                        if board[j + (i * 3)][k + (f * 3)] != ".":
+                            dicti[board[j + (i * 3)][k + (f * 3)]] += 1
+                        if(dicti[board[j + (i * 3)][k + (f * 3)]] > 1):
+                            print(dicti, dictj)
+                            print("2")
+                            return False
+        return True
